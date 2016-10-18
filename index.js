22ef42b761c3c3d637afbdab6b07d62d50661124
Piotr Karpala <pkarpala@infusion.com>
2016-10-17 20:49:42 -0400

    code cleanup

--- index.js
+++ index.js
@@ -4,29 +4,28 @@
 module.exports = function deepEqual(a, b) {
     if (typeof a !== typeof b) {
         return false;
-    } else {
-        if (typeof a === 'object') {
-            if (a && b && Object.keys(a).length != Object.keys(b).length) {
-                return false;
-            }
+    } else if (typeof a === 'object') {
 
-            for (let p in a) {
-                if (!deepEqual(a[p], b[p])) {
-                    return false;
-                }
-            }
-
-            if (a === null && b !== null || (b === null && a !== null)) {
-                return false;
-            }
-
-            if (a && b && Object.getPrototypeOf(a) != Object.getPrototypeOf(b)) {
-                return false;
-            }
-
-        } else if (a !== b) {
+        if (a && b && Object.keys(a).length != Object.keys(b).length) {
             return false;
         }
+
+        for (let p in a) {
+            if (!deepEqual(a[p], b[p])) {
+                return false;
+            }
+        }
+
+        if (a === null && b !== null || (b === null && a !== null)) {
+            return false;
+        }
+
+        if (a && b && Object.getPrototypeOf(a) != Object.getPrototypeOf(b)) {
+            return false;
+        }
+
+    } else if (a !== b) {
+        return false;
     }
 
     return true;
